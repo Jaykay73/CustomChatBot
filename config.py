279ef4499ffe2dfb7363cdceb58930792a80b98c
Jaykay73 <127900648+Jaykay73@users.noreply.github.com>
2025-08-14 01:30:27 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,4 @@
-import os
-from dotenv import load_dotenv
+import streamlit as st
 
-load_dotenv()
-OPENROUTER_API_KEY = os.getenv("OPENROUTER_API_KEY")
+OPENROUTER_API_KEY = st.secrets["OPENROUTER_API_KEY"]
 
